48197caade65ba20cddace7b9df1529b2d785ac5
skullviper <80700838+pratikraj244@users.noreply.github.com>
2025-03-28 14:06:49 +0530

    Create overview.py

---
+++ overview.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.set_page_config(
+    page_title="4cast",
+    layout="wide"
+)
+page = st.sidebar.radio("Go to", ["about","dataset", "insights", "prediction"])
+from pages import about,dataset, insights, prediction
+# Load the appropriate page
+if page == "about":
+    about.show()
+elif page == "dataset":
+    dataset.main()
+elif page == "insights":
+    insights.main()
+elif page == "prediction":
+    prediction.main()
